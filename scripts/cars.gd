b31a27d8a26c259ca951f8fd437edf8f1fe78c5d
Cherry <chenniexan@gmail.com>
2026-06-18 22:12:01 +0800

    Updated: Cars moving

---
+++ scripts/cars.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed = 150.0
+@export var direction = 1  # 1 = right, -1 = left
+@export var car_col = 0    # 0-3 (which column)
+@export var car_row = 0    # 0-2 (which row)
+
+const CAR_WIDTH = 102
+const CAR_HEIGHT = 203
+
+func _ready():
+	$Sprite2D.region_enabled = true
+	$Sprite2D.region_rect = Rect2(
+		car_col * CAR_WIDTH,
+		car_row * CAR_HEIGHT,
+		CAR_WIDTH,
+		CAR_HEIGHT
+	)
+	if direction == 1:
+		$Sprite2D.rotation_degrees = 90   # facing right
+	elif direction == -1:
+		$Sprite2D.rotation_degrees = -90  # facing left
+
+func _physics_process(delta):
+	position.x += speed * direction * delta
+	if direction == 1 and position.x > 1200:
+		position.x = -50
+	elif direction == -1 and position.x < -50:
+		position.x = 1200
